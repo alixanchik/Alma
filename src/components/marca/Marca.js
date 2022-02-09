ed5cadaf08bf038cd4ce13095b44720d45fb42d5
a1ixanchik <kurmanbekovalihan81@gmail.com>
2022-02-09 14:58:16 +0300

    asfg

--- src/components/marca/Marca.js
+++ src/components/marca/Marca.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import "../marca/Marca.css"
-import aenti from "./marca.img/aetna.png"
-import oscar from "./marca.img/jscar.png"
-import united from "./marca.img/united.png"
-import oxford from "./marca.img/oscar.png"
-import optum from "./marca.img/optum.png"
-import yarvard from "./marca.img/harvard.png"
+import aenti from "../../img/Rectangle (1).png"
+// import oscar from "./marca.img/jscar.png"
+// import united from "./marca.img/united.png"
+// import oxford from "./marca.img/oscar.png"
+// import optum from "./marca.img/optum.png"
+// import yarvard from "./marca.img/harvard.png"
 function Marca() {
     return (
         <div>
